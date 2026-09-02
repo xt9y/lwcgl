9de8d2d28980512fe5a38acf93f31805a3aa011e
xt9y <felix@xt9y.de>
2026-09-02 23:32:15 +0200

    lwcgl 2.9.3: add ContextAttribs-compatible context API

---
+++ include/lwcgl/context.h
@@ -0,0 +1,27 @@
+#ifndef LWCGL_CONTEXT_H
+#define LWCGL_CONTEXT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum {
+    LWCGL_CONTEXT_ANY_PROFILE = 0,
+    LWCGL_CONTEXT_CORE_PROFILE = 1,
+    LWCGL_CONTEXT_COMPATIBILITY_PROFILE = 2
+};
+
+/* Native equivalent of LWJGL 2 ContextAttribs for Display creation. */
+void lwcglSetContextVersion(int major, int minor);
+void lwcglSetContextProfile(int profile);
+void lwcglSetContextDebug(int enabled);
+int lwcglRequestedContextMajorVersion(void);
+int lwcglRequestedContextMinorVersion(void);
+int lwcglRequestedContextProfile(void);
+int lwcglRequestedContextDebug(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
