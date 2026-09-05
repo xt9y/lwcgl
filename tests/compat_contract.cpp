edbc77e221ca5f0f0d6044a29f49705e7ec5c4d8
xt9y <felix@xt9y.de>
2026-09-05 20:37:45 +0200

    fix v2 sanitizer contract leak

--- tests/compat_contract.cpp
+++ tests/compat_contract.cpp
@@ -1,8 +1,10 @@
 #define LWCGL_ENABLE_LWJGL2_COMPAT 1
 #include <lwcgl/lwcgl.h>
 int main() {
-    Display.setDisplayMode(new DisplayMode(320, 200));
+    LWCGLDisplayModeValue mode = {320, 200, 0, 0, LWCGL_FALSE};
+    Display.setDisplayMode(&mode);
     FloatBuffer *m = BufferUtils.createFloatBuffer(16);
+    if (!m) return 1;
     glLoadMatrix(m);
     BufferUtils.destroy(m);
     return 0;
